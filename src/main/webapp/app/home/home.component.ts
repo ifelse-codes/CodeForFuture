a74c80ebbcd5977d37ccb0421ef7f339be8fcf13
Suman Patra <sumanpatra@decorpot.com>
2020-04-04 01:25:29 +0530

    [code] add angular presentation

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { LoginModalService } from 'app/core/login/login-modal.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
+
+export enum KEY_CODE {
+  RIGHT_ARROW = 39,
+  LEFT_ARROW = 37
+}
 
 @Component({
   selector: 'jhi-home',
@@ -13,6 +18,8 @@ import { Account } from 'app/core/user/account.model';
 export class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
   authSubscription?: Subscription;
+  pages: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  currentPage = 1;
 
   constructor(private accountService: AccountService, private loginModalService: LoginModalService) {}
 
@@ -31,6 +38,38 @@ export class HomeComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     if (this.authSubscription) {
       this.authSubscription.unsubscribe();
+    }
+  }
+
+  next(): void {
+    // eslint-disable-next-line no-console
+    console.log('next ');
+    if (this.currentPage === this.pages[this.pages.length - 1]) {
+      this.currentPage = this.pages[0];
+    } else {
+      this.currentPage++;
+    }
+  }
+
+  prev(): void {
+    // eslint-disable-next-line no-console
+    console.log('prev');
+
+    if (this.currentPage === this.pages[0]) {
+      this.currentPage = this.pages[this.pages.length - 1];
+    } else {
+      this.currentPage--;
+    }
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent): void {
+    if (event.keyCode === KEY_CODE.RIGHT_ARROW) {
+      this.next();
+    }
+
+    if (event.keyCode === KEY_CODE.LEFT_ARROW) {
+      this.prev();
     }
   }
 }
